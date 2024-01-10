4b41646fe57368e9d42050630685b96389c46b7e
Clément Guillemot <125440005+GuillemotClement@users.noreply.github.com>
2024-01-10 17:27:52 +0100

    Bug sur la vérif formulaire.

--- submit_contact.php
+++ submit_contact.php
@@ -3,12 +3,15 @@ include_once("head.php");
 
 $getData = $_GET;
 
-if(!isset($getDate['email']) 
-    || filter_var($getData['email'], FILTER_VALIDATE_EMAIL)
-    || empty($getData['message'])
-    || trim($getData['message']))
+if( !isset($getDate['email']) )
+    // || !filter_var($getData['email'], FILTER_VALIDATE_EMAIL)
+    // || empty($getData['message']))
+    // || trim($getData['message']) === '')
     {
         echo "<h1>Erreur</h1>";
         echo "<p>Saisis invalide";
+    }
+    else {
+        echo print_r($getData);
     }
 include_once('footer.php');
