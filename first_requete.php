2326bf8e1a159e78dd06436deaa99f0d6d07545e
Clément Guillemot <125440005+GuillemotClement@users.noreply.github.com>
2024-01-17 16:27:44 +0100

    Ajout des requetes pour recuperer les donnes de la BDD

--- first_requete.php
+++ first_requete.php
@@ -1,17 +1,24 @@
 <?php
-try
-{
-    $connexion = new PDO("mysql:host=localhost;dbname=recette_magique;charset=utf8",'root','');
-}
-catch(Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
 
+require_once('databaseconnect.php');
 
+// recuperation des element de la table users;
+$queryUser = "SELECT * FROM users";
+$usersStatement = $mysqlClient->prepare($queryUser);
+$usersStatement->execute();
+$users = $usersStatement->fetchAll(PDO::FETCH_ASSOC);
+
+// Recuperation des element de la table recipe
+$queryRecipe = "SELECT * FROM recipes WHERE available = true";
+$recipeStatement = $mysqlClient->prepare($queryRecipe);
+$recipeStatement->execute();
+$recipes = $recipeStatement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
 <pre>
     <?php print_r($users);?>
+</pre>
+<pre>
+    <?php print_r($recipes);?>
 </pre>
