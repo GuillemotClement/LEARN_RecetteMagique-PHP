39a47ecc870891317d74a8d61b50c4653f19ff93
Clément Guillemot <125440005+GuillemotClement@users.noreply.github.com>
2024-01-17 16:10:01 +0100

    Ajout du fichier des info de connection a la db dans mysql.php

--- mysql.php
+++ mysql.php
@@ -1,8 +1,7 @@
 <?php 
 
-try{
-    $mysqlClient = new PDO ('mysql:host=localhost;dbname=recette_magique;utf*', 'root', '');
-}catch(Exception $e){
-    die('Erreur : ' . $e->getMessage());
-}
+$host = 'localhost';
+$dbname = 'recette_magique';
+$user = 'root';
+$password = '';
 
